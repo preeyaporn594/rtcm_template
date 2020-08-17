5de2f00a84bfbf4bacdea47df162ee7eca0dc1e0
preeyaporn594 <61227554+preeyaporn594@users.noreply.github.com>
2020-08-17 15:35:05 +0700

    add new files
    add a bunch of new files

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RTCM+</title>
+    <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet">
+    <!---css-------->
+    <link rel="stylesheet" href="styles.css">
+</head>
+    <body>
+        <header>
+            <div class="overlay">
+                <h1>RTCMPLUS +</h1>
+                <p class="p1">Real Time Cohort Monitoring</p>
+                <p class="p2">( กองโรคเอดส์และโรคติดต่อทางเพศสัมพันธ์ )</p>
+            <!-- <button> open </button>-->
+            </div>
+        </header>
+        <section class="box-card">
+            <div class="box-box">
+                
+                    <div class="card1">
+                        <div class="dot"></div>
+                        <div class="dot two"></div>
+
+                        <div class="icon1">
+                            <img class="icon1-1" src="images/icon4.svg">
+                        </div>
+
+                        <div class="message">
+                            <h1 class="mge1">DASHBOARD</h1>
+                            <p>สำหรับดูผลการดำเนินงานและรายงาน</p>
+                        </div>
+                        
+                        <button class="button-d">
+                            <h1 class="d1">เปิด</h1>
+                        </button>
+                    </div>
+                
+
+              
+                    <div class="card2">
+                        <div class="dot"></div>
+                        <div class="dot two"></div>
+
+                        <div class="icon1">
+                            <img class="icon1-2" src="images/icon6.svg">
+                        </div>
+
+                        <div class="message">
+                            <h1 class="mge1">WEBAPP</h1>
+                            <p>สำหรับบันทึกข้อมูลผู้รับบริการ</p>
+                        </div>
+                        
+                        <button class="button-d">
+                            <h1 class="d2">เปิด</h1>
+                        </button>
+                    </div>
+                
+            </div>
+        </section>
+    </body>
+</html>
